3b56df3cbefda6b6db9275437753068b9cfed333
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-05-13 09:13:00 -0500

    more content added, mobile responsive

--- src/components/mainpages/home/Home.js
+++ src/components/mainpages/home/Home.js
@@ -2,19 +2,27 @@ import React from 'react';
 import './Home.css'
 import Merle from '../../../img/pups2.jpg'
 // import TwoDogs from '../../../img/IMG_7224.heic'
+import AKC from '../../../img/akc.svg'
 
 
 function Home() {
     return (
         <section className="home-section">
-           <div className='container-box'>
-           
-           <img className='merle-dog' src={Merle}/>
 
+           <div className='container-box'>
+           <img className='merle-dog' alt='' src={Merle}/>
            <div className="centered-text"> #LivingTheFrenchieLife </div>
-          
            </div>
-          
+
+           <div className='about-us'>
+           <h2>Welcome to Go Frenchie</h2>
+           <p1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p1>
+           </div>
+
+           <div className='akc-section'>
+           <img className='akc-svg' alt='' src={AKC} width='100px'/>
+           <p className='akc-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+          </div>
          
         </section>
     );
