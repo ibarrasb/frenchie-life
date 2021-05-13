4207e6f02659334bcfbafb0f1e26fdab45de2972
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-05-13 09:59:02 -0500

    akc section added, mobile responsive

--- src/components/mainpages/home/Home.js
+++ src/components/mainpages/home/Home.js
@@ -15,12 +15,13 @@ function Home() {
            </div>
 
            <div className='about-us'>
-           <h2>Welcome to Go Frenchie</h2>
+           <h2 className="headline">Welcome to Go Frenchie</h2>
            <p1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p1>
            </div>
 
            <div className='akc-section'>
            <img className='akc-svg' alt='' src={AKC} width='100px'/>
+           <h3 className="headline akc-title">Quality AKC DNA Pups</h3>
            <p className='akc-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
           </div>
          
